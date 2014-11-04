3895e08908272fb1c7d61e0f45610e64e6b43b99
Roman Kovalenko <roman.kovalenko88gmail.com>
2014-11-04 21:10:25 +0200

    apache fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once(dirname(__FILE__) . '/protected/config/environment.php');
-$environment = new Environment('DEV');
+$environment = new Environment();
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG', $environment->getDebug());
