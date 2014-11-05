5231275edbd85a3e9b08e684775fbe5c618841ce
Roman Kovalenko <roman.kovalenko88gmail.com>
2014-11-05 20:53:06 +0200

    ...

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,9 +1,8 @@
 <?php /* @var $this Controller */ ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo Yii::app()->language; ?>"
-      lang="<?php echo Yii::app()->language; ?>">
+<!DOCTYPE html>
+<html lang="<?php echo Yii::app()->language; ?>">
 <head>
+	<meta charset="utf-8">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="language" content="<?php echo Yii::app()->language; ?>"/>
 
