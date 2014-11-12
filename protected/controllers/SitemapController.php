b3856948a8e250636907be44e9ce0850c6d2f639
Roman Kovalenko <roman.kovalenko88gmail.com>
2014-11-13 01:01:49 +0200

    fix

--- protected/controllers/SitemapController.php
+++ protected/controllers/SitemapController.php
@@ -7,32 +7,32 @@ class SitemapController extends Controller
 
 	public function actionIndex()
 	{
-//		if (!$xml = Yii::app()->cache->get('sitemap')) {
+		if (!$xml = Yii::app()->cache->get('sitemap')) {
 
 			$sitemap = new DSitemap();
 
 			$sitemap->addUrl('/', DSitemap::DAILY);
 
-		$languages = Yii::app()->params->languages;
+			$languages = Yii::app()->params->languages;
 
-		$host = Yii::app()->request->hostInfo;
-		foreach ($languages as $key => $lang) {
-			$lang_map[] = array(
-				'hreflang' => $key,
-				'href' => $host . '/' . $key
-			);
-		}
+			$host = Yii::app()->request->hostInfo;
 
-		foreach ($languages as $lang_key => $value) {
+			foreach ($languages as $key => $lang) {
+				$lang_map[] = array(
+					'hreflang' => $key,
+					'href' => $host . '/' . $key
+				);
+			}
 
-			$sitemap->addUrlLang('/', $lang_map, $lang_key, DSitemap::DAILY, 1);
-			$sitemap->addUrlLang('/site/page/view/about', $lang_map, $lang_key, DSitemap::WEEKLY, 0.5);
-			$sitemap->addUrlLang('/contact', $lang_map, $lang_key, DSitemap::NEVER, 0.3);
+			foreach ($languages as $lang_key => $value) {
+				$sitemap->addUrlLang('/', $lang_map, $lang_key, DSitemap::DAILY, 1);
+				$sitemap->addUrlLang('/site/page/view/about', $lang_map, $lang_key, DSitemap::WEEKLY, 0.5);
+				$sitemap->addUrlLang('/contact', $lang_map, $lang_key, DSitemap::NEVER, 0.3);
 			}
 
 			$xml = $sitemap->render();
-//			Yii::app()->cache->set('sitemap', $xml, self::KEEP_DELAY);
-//		}
+			Yii::app()->cache->set('sitemap', $xml, self::KEEP_DELAY);
+		}
 
 		header("Content-type: text/xml");
 		echo $xml;
