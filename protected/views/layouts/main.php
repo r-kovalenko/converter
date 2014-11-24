78203f7cf185ba615472c77aa738f8ef129f9488
Roman Kovalenko <roman.kovalenko88gmail.com>
2014-11-25 01:03:50 +0200

    liveinternet

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -125,6 +125,18 @@ $language = Yii::app()->getLanguage();
 			Copyright &copy; <?php echo date('Y'); ?>.<br/>
 			<?php echo Yii::t('translate', 'All Rights Reserved'); ?>.<br/>
 		</div>
+		<!--LiveInternet counter-->
+		<script type="text/javascript"><!--
+			document.write("<a href='//www.liveinternet.ru/click' " +
+			"target=_blank><img src='//counter.yadro.ru/hit?t42.10;r" +
+			escape(document.referrer) + ((typeof(screen) == "undefined") ? "" :
+			";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+				screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+			";h" + escape(document.title.substring(0, 80)) + ";" + Math.random() +
+			"' alt='' title='LiveInternet' " +
+			"border='0' width='31' height='31'><\/a>")
+			//--></script>
+		<!--/LiveInternet-->
 	</div>
 	<!-- footer -->
 
